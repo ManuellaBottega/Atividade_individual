990c4b06de21c35a8bcb7d7ab0d423ef4f04e5cc
ManuellaBottega <manuelladasnevesbottega@gmail.com>
2025-08-05 15:46:13 -0300

    ajuste

--- adicionar.js
+++ adicionar.js
@@ -4,7 +4,7 @@ function adicionarContatos (contatos, prompt, menu){
     if (isNaN(telefone)) {
         console.log('Numero invalido, tente novamente.\n')
         adicionarContatos(contatos, prompt, menu)
-    }
+    } else {
     const email = prompt('Email: ')
     const Id = contatos.length > 0 ? contatos[contatos.length - 1].id + 1 : 1
     const contato = {
@@ -15,7 +15,7 @@ function adicionarContatos (contatos, prompt, menu){
     }
     contatos.push(contato)
     console.log('\nContato adicionado com sucesso!')
-
+    }
     console.log ('\nPressione enter para retornar ao menu, ou s para adicionar outro contato.')
     let voltarMenu = prompt ('')
     if (voltarMenu == '') {
